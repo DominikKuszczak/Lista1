45ea650ef0f4dd0d33da40a3bdb79e7ba3f24ece
Dominik <dominik.kuszczak@gmail.com>
2015-10-03 12:50:47 +0200

    First commit

---
+++ src/LiczbaException.java
@@ -0,0 +1,12 @@
+public class LiczbaException extends Exception 
+{
+        /**
+         * 
+         */
+        private static final long serialVersionUID = 1L;
+
+        LiczbaException(String s)
+        {
+                super(s);
+        };
+};
